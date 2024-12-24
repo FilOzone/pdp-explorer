7d90226fa4ed006145c51e07c643db4012485017
silent-cipher <puspendramahariya@gmail.com>
2024-12-24 17:32:43 +0530

    initialize frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>PDP Explorer</h1>
+        <p className={styles.description}>
+          Explore the PDP hot storage network
+        </p>
+      </main>
+    </div>
+  );
+}
