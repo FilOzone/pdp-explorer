53ce6c4aba45fbee5d2d688979cd28e6b8452730
kaola526 <huweixiong@storswift.com>
2026-04-02 06:09:14 +0800

    Update subgraph-client/src/hooks/usePiecePageData.tsx
    Co-authored-by: Puspendra Mahariya <95584952+silent-cipher@users.noreply.github.com>

--- subgraph-client/src/hooks/usePiecePageData.tsx
+++ subgraph-client/src/hooks/usePiecePageData.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { parseCidToHex } from '@/utility/helper'
 import useGraphQL from './useGraphQL'
 import { pieceDetailsQuery } from '@/utility/queries'
